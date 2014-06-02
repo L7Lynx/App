00f4daf29c5b266c674268c204b334b03843e210
KarelStevens <karel_stevens@hotmail.com>
2014-06-02 13:09:42 +0200

    redirects problemen

--- js/myController.js
+++ js/myController.js
@@ -2,6 +2,8 @@
  * Created by KarelStevens on 2/06/2014.
  */
 
-$('#loginbtn').click(function(){
-    window.location.href = "http://www.google.com";
-})
+function buttonClicked()
+{
+    window.location='partials/home.html';
+    alert("hey2");
+}
